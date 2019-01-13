73cf0c15142a85e5d97386636ba961a21259bdba
Micke M <46315517+Lai1337@users.noreply.github.com>
2019-01-13 15:49:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ class Nexa433(ActorBase):
     def on(self, power=100):
         try:
             print self.plug
-            command = "/home/pi/craftbeerpi3/modules/plugins/Nexa433/actor-on.sh " + str(self.plug)
+            command = "/home/pi/actor-on.sh " + str(self.plug)
             subprocess.call(command, shell=True)
         except Exception as e:
             self.api.notify("Error Nexa433 plugin", type="danger", timeout=None)
@@ -21,7 +21,7 @@ class Nexa433(ActorBase):
 
     def off(self):
         try:
-            command = "/home/pi/craftbeerpi3/modules/plugins/Nexa433/actor-off.sh " + str(self.plug)
+            command = "/home/pi/actor-off.sh " + str(self.plug)
             subprocess.call(command, shell=True)
         except Exception as e:
             self.api.notify("Error Nexa 433 plugin", type="danger", timeout=None)
